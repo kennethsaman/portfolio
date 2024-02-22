a0b1b4fd57a034ab3e5e4cd82f9da97e8b3cdf28
kennethsaman <kennethsaman@gmail.com>
2024-02-23 04:44:04 +0800

    fixed modal handler. Cleaned up code by removing console.log statements

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,38 +19,24 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 }))
 
 
-// GALLERY HANDLER
-
-function getImages () {
-  console.log("getImages called");
-
-  $(".gallery").append(
-    "<img src='./assets/images/content/r (1).png' alt=''>"
-  );
-  
-  console.log("getImages end");
-}
-
-
-
 
 
 // MODAL IMAGE HANDLER
 
-function setSource (imgsrc) {
-  document.querySelector(".modal-image").src = imgsrc;
-  console.log(imgsrc + "from setSource function");
-}
+// function setSource (imgsrc) {
+//   document.querySelector(".modal-image").src = imgsrc;
+//   console.log(imgsrc + "from setSource function");
+// }
 
-document.querySelectorAll(".item").forEach(n => n.addEventListener("click", () => {
-  img = n.getElementsByTagName("img");
-  console.log(img[0].src);
-  setSource (img[0].src);
-  modal.classList.toggle("active");
-  modal.addEventListener("click", () => {
-    modal.classList.remove("active");
-  })
-}))
+// document.querySelectorAll(".item").forEach(n => n.addEventListener("click", () => {
+//   img = n.getElementsByTagName("img");
+//   console.log(img[0].src);
+//   setSource (img[0].src);
+//   modal.classList.toggle("active");
+//   modal.addEventListener("click", () => {
+//     modal.classList.remove("active");
+//   })
+// }))
 
 
 
